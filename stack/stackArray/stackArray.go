27433b99a762e0f5a8cfae511a28cc4d391372bb
Turkington, Kevin <kevin.turkington@nordstrom.com>
2019-10-23 15:07:04 -0700

    rename stack array

--- stack/stackArray/stackArray.go
+++ stack/stackArray/stackArray.go
@@ -1,4 +1,4 @@
-package stackArray
+package stackarray
 
 // StackArray generic stack implementation using a array
 type StackArray struct {
--- stack/stackArray/stackarray_test.go
+++ stack/stackArray/stackarray_test.go
@@ -1,4 +1,4 @@
-package stackArray
+package stackarray
 
 import (
 	"testing"
