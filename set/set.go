fef1f09063f0c44e9985a9b1bf1cf3f70531a629
Turkington, Kevin <kevin.turkington@nordstrom.com>
2019-10-24 16:16:56 -0700

    adding set and testing for set

--- set/set.go
+++ set/set.go
@@ -5,4 +5,58 @@ type Empty struct{}
 
 // Set generic hashmap implementation
 type Set struct {
+	Data map[interface{}]Empty
+}
+
+// New create Set
+func New() *Set {
+	return &Set{
+		Data: make(map[interface{}]Empty),
+	}
+}
+
+// Add insert key to set
+// O(1)
+func (s *Set) Add(key interface{}) {
+	s.Data[key] = Empty{}
+}
+
+// Has check if key is in set
+// O(1)
+func (s *Set) Has(key interface{}) bool {
+	_, ok := s.Data[key]
+	return ok
+}
+
+// Delete remove key from set
+// O(1)
+func (s *Set) Delete(key interface{}) {
+	delete(s.Data, key)
+}
+
+// Len return number of elements in set
+// O(1)
+func (s *Set) Len() int {
+	return len(s.Data)
+}
+
+// Keys generate array of keys in set, unordered
+// O(N)
+func (s *Set) Keys() []interface{} {
+	result := make([]interface{}, 0, 0)
+
+	for key := range s.Data {
+		result = append(result, key)
+	}
+
+	return result
+}
+
+// ForEach iterate over all elements in set, unordered
+// callback func(key interface{})
+// O(N)
+func (s *Set) ForEach(callback func(key interface{})) {
+	for key := range s.Data {
+		callback(key)
+	}
 }
--- set/set_test.go
+++ set/set_test.go
@@ -1,1 +1,104 @@
 package set
+
+import "testing"
+
+func TestAddToSet(t *testing.T) {
+	s := New()
+
+	s.Add(1)
+	if _, ok := s.Data[1]; !ok {
+		t.Errorf("set did not add a new key")
+	}
+	s.Add("test")
+	if _, ok := s.Data["test"]; !ok {
+		t.Errorf("set did not add a new key")
+	}
+}
+
+func TestHasInSet(t *testing.T) {
+	s := New()
+	s.Add(1)
+	s.Add("test")
+
+	if !s.Has(1) {
+		t.Errorf("set should contain key 1")
+	}
+	if !s.Has("test") {
+		t.Errorf("set should contain key \"test\"")
+	}
+}
+
+func TestDeleteInSet(t *testing.T) {
+	s := New()
+
+	s.Add(1)
+	s.Delete(1)
+	if _, ok := s.Data[1]; ok {
+		t.Errorf("set did not delete key")
+	}
+
+	s.Add("test")
+	s.Delete("test")
+	if _, ok := s.Data["test"]; ok {
+		t.Errorf("set did not delete key")
+	}
+}
+
+func TestLenSet(t *testing.T) {
+	s := New()
+
+	s.Add(1)
+	if s.Len() != 1 {
+		t.Errorf("Length was not correct")
+	}
+
+	s.Add("test")
+	if s.Len() != 2 {
+		t.Errorf("Length was not correct")
+	}
+}
+
+func TestKeysInSet(t *testing.T) {
+	s := New()
+	s.Add(1)
+	s.Add("test")
+
+	keys := s.Keys()
+	if v, ok := keys[0].(string); ok && v != "test" {
+		t.Errorf("key \"test\" was not saved to array")
+	}
+	if v, ok := keys[0].(int); ok && v != 1 {
+		t.Errorf("key 1 was not saved to array")
+	}
+	if v, ok := keys[1].(string); ok && v != "test" {
+		t.Errorf("key \"test\" was not saved to array")
+	}
+	if v, ok := keys[1].(int); ok && v != 1 {
+		t.Errorf("key 1 was not saved to array")
+	}
+}
+
+func TestForEachInSet(t *testing.T) {
+	s := New()
+	s.Add(1)
+	s.Add("test")
+
+	callCount := 0
+
+	callback := func(key interface{}) {
+		if v, ok := key.(string); ok && v != "test" {
+			t.Errorf("key \"test\" was not saved to array")
+		}
+		if v, ok := key.(int); ok && v != 1 {
+			t.Errorf("key 1 was not saved to array")
+		}
+
+		callCount++
+	}
+
+	s.ForEach(callback)
+
+	if callCount != 2 {
+		t.Errorf("callback function was not called in forEach loop")
+	}
+}
