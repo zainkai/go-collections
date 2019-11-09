95b7fc3246a86b90d7901d3eafb888db5b24d56a
Kevin Turkington <turkingtonkevin@gmail.com>
2019-11-08 18:55:41 -0800

    all traversals

--- bst/traversal.go
+++ bst/traversal.go
@@ -1,5 +1,9 @@
 package bst
 
+import (
+	"github.com/zainkai/go-collections/stack"
+)
+
 func (tree *BST) InOrderRec(f func(key, data interface{})) {
 	tree.Head.inOrderRec(f)
 }
@@ -14,23 +18,88 @@ func (n *NodeBST) inOrderRec(f func(key, data interface{})) {
 	n.Right.inOrderRec(f)
 }
 
-// func (tree *BST) InOrder(f func(key, data interface{})) {
-// 	stk := stack.New()
-// 	stk.Push(tree.Head)
+func (tree *BST) InOrder(f func(key, data interface{})) {
+	stk := stack.New()
+	temp := tree.Head
 
-// 	for stk.Len() > 0 {
-// 		tmp := stk.Peek().(*NodeBST)
-// 		if tmp.Left != nil {
-// 			stk.Push(tmp.Left)
-// 			continue
-// 		} else {
-// 			f(tmp.Key, tmp.Data)
-// 			stk.Pop()
+	for temp != nil || stk.Len() > 0 {
+		if temp != nil {
+			stk.Push(temp)
+			temp = temp.Left
+			continue
+		}
+		temp = stk.Pop().(*NodeBST)
+		f(temp.Key, temp.Data)
 
-// 			if tmp.Right != nil {
-// 				stk.Push(tmp.Right)
-// 			}
-// 		}
+		temp = temp.Right
+	}
+}
 
-// 	}
-// }
+func (tree *BST) PreOrderRec(f func(key, data interface{})) {
+	tree.Head.preOrderRec(f)
+}
+
+func (n *NodeBST) preOrderRec(f func(key, data interface{})) {
+	if n == nil {
+		return
+	}
+
+	f(n.Key, n.Data)
+	n.Left.preOrderRec(f)
+	n.Right.preOrderRec(f)
+}
+
+func (tree *BST) PreOrder(f func(key, data interface{})) {
+	stk := stack.New()
+	stk.Push(tree.Head)
+
+	for stk.Len() > 0 {
+		temp := stk.Pop().(*NodeBST)
+		f(temp.Key, temp.Data)
+
+		if temp.Right != nil {
+			stk.Push(temp.Right)
+		}
+		if temp.Left != nil {
+			stk.Push(temp.Left)
+		}
+	}
+}
+
+func (tree *BST) PostOrderRec(f func(key, data interface{})) {
+	tree.Head.postOrderRec(f)
+}
+
+func (n *NodeBST) postOrderRec(f func(key, data interface{})) {
+	if n == nil {
+		return
+	}
+
+	n.Left.postOrderRec(f)
+	n.Right.postOrderRec(f)
+	f(n.Key, n.Data)
+}
+
+// PostOrder 2 stack solution
+func (tree *BST) PostOrder(f func(key, data interface{})) {
+	stk := stack.New()
+	stk.Push(tree.Head)
+	outputStk := stack.New()
+
+	for stk.Len() > 0 {
+		temp := stk.Pop().(*NodeBST)
+		outputStk.Push(temp)
+
+		if temp.Left != nil {
+			stk.Push(temp.Left)
+		}
+		if temp.Right != nil {
+			stk.Push(temp.Right)
+		}
+	}
+
+	for outputStk.Len() > 0 {
+		temp := outputStk.Pop().(*NodeBST)
+		f(temp.Key, temp.Data)
+	}
+}
---
+++ bst/traversal_test.go
@@ -0,0 +1,138 @@
+package bst
+
+import "testing"
+
+func TestInOrderTraversalRecursive(t *testing.T) {
+	initialBst := []int{5, 3, 7, 2, 4, 6, 8, 1}
+	bst := New(isLessInt, isEqualInt)
+	for _, key := range initialBst { // populate tree
+		bst.Insert(key, nil)
+	}
+
+	traversalResult := []int{}
+	visitNode := func(k, d interface{}) {
+		traversalResult = append(traversalResult, k.(int))
+	}
+
+	bst.InOrderRec(visitNode)
+
+	expectedResult := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	for i, expected := range expectedResult {
+		if traversalResult[i] != expected {
+			t.Errorf("expected traversalResult at index: %d to equal: %d instead got %d", i, expected, traversalResult[i])
+		}
+	}
+}
+
+func TestInOrderTraversalIterative(t *testing.T) {
+	initialBst := []int{5, 3, 7, 2, 4, 6, 8, 1}
+	bst := New(isLessInt, isEqualInt)
+	for _, key := range initialBst { // populate tree
+		bst.Insert(key, nil)
+	}
+
+	traversalResult := []int{}
+	visitNode := func(k, d interface{}) {
+		if k == nil {
+			return
+		}
+		traversalResult = append(traversalResult, k.(int))
+	}
+
+	bst.InOrder(visitNode)
+
+	expectedResult := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	for i, expected := range expectedResult {
+		if traversalResult[i] != expected {
+			t.Errorf("expected traversalResult at index: %d to equal: %d instead got %d", i, expected, traversalResult[i])
+		}
+	}
+}
+
+func TestPreOrderTraversalRecursive(t *testing.T) {
+	initialBst := []int{5, 3, 7, 2, 4, 6, 8, 1}
+	bst := New(isLessInt, isEqualInt)
+	for _, key := range initialBst { // populate tree
+		bst.Insert(key, nil)
+	}
+
+	traversalResult := []int{}
+	visitNode := func(k, d interface{}) {
+		traversalResult = append(traversalResult, k.(int))
+	}
+
+	bst.PreOrderRec(visitNode)
+
+	expectedResult := []int{5, 3, 2, 1, 4, 7, 6, 8}
+	for i, expected := range expectedResult {
+		if traversalResult[i] != expected {
+			t.Errorf("expected traversalResult at index: %d to equal: %d instead got %d", i, expected, traversalResult[i])
+		}
+	}
+}
+
+func TestPreOrderTraversalIterative(t *testing.T) {
+	initialBst := []int{5, 3, 7, 2, 4, 6, 8, 1}
+	bst := New(isLessInt, isEqualInt)
+	for _, key := range initialBst { // populate tree
+		bst.Insert(key, nil)
+	}
+
+	traversalResult := []int{}
+	visitNode := func(k, d interface{}) {
+		traversalResult = append(traversalResult, k.(int))
+	}
+
+	bst.PreOrder(visitNode)
+
+	expectedResult := []int{5, 3, 2, 1, 4, 7, 6, 8}
+	for i, expected := range expectedResult {
+		if traversalResult[i] != expected {
+			t.Errorf("expected traversalResult at index: %d to equal: %d instead got %d", i, expected, traversalResult[i])
+		}
+	}
+}
+
+func TestPostOrderTraversalRecursive(t *testing.T) {
+	initialBst := []int{5, 3, 7, 2, 4, 6, 8, 1}
+	bst := New(isLessInt, isEqualInt)
+	for _, key := range initialBst { // populate tree
+		bst.Insert(key, nil)
+	}
+
+	traversalResult := []int{}
+	visitNode := func(k, d interface{}) {
+		traversalResult = append(traversalResult, k.(int))
+	}
+
+	bst.PostOrderRec(visitNode)
+
+	expectedResult := []int{1, 2, 4, 3, 6, 8, 7, 5}
+	for i, expected := range expectedResult {
+		if traversalResult[i] != expected {
+			t.Errorf("expected traversalResult at index: %d to equal: %d instead got %d", i, expected, traversalResult[i])
+		}
+	}
+}
+
+func TestPostOrderTraversalIterative(t *testing.T) {
+	initialBst := []int{5, 3, 7, 2, 4, 6, 8, 1}
+	bst := New(isLessInt, isEqualInt)
+	for _, key := range initialBst { // populate tree
+		bst.Insert(key, nil)
+	}
+
+	traversalResult := []int{}
+	visitNode := func(k, d interface{}) {
+		traversalResult = append(traversalResult, k.(int))
+	}
+
+	bst.PostOrder(visitNode)
+
+	expectedResult := []int{1, 2, 4, 3, 6, 8, 7, 5}
+	for i, expected := range expectedResult {
+		if traversalResult[i] != expected {
+			t.Errorf("expected traversalResult at index: %d to equal: %d instead got %d", i, expected, traversalResult[i])
+		}
+	}
+}
